f9ef9b4f4d858eca2b0379050a58bced67360509
chef-vulcan <lilcrystalhimself@gmail.com>
2025-07-27 10:45:06 +0100

    added the file 6-multi_languages_loop.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,3 +1,6 @@
+//Script that prints My number: <first argument converted in integer> if the first argument can
+//be converted to an integer
+
 
 const args = process.argv[2]; // ingnore the first two elements (node and script path)
 
---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+//A script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i=0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
+// The code iterates through an array of strings and logs each string to the console
+// It uses a for loop to access each element of the array and print it
